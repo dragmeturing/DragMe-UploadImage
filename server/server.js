eb3631152a5689f65386ed0d1b8ae09bc21b481c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-16 17:29:50 -0600

    Remove app.set

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 3001;
 
-app.set('port', port);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} 🏃🏼‍🏃🏼‍🏃🏼‍`)
