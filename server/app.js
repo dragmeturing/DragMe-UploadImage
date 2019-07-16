9ed86b6fed9d6e7b7137d7d13a4ff59681218fd1
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-16 17:25:07 -0600

    Update app

--- server/app.js
+++ server/app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require('cors')
-const environment = process.env.NODE_ENV || "development";
-const configuration = require("../knexfile")[environment];
-const database = require("knex")(configuration);
-const key = require('../key')
 
 app.use(bodyParser.json());
 app.use(cors())
