603ab2d501a411bcab26001483d55143c3a229f1
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-16 18:19:25 -0600

    Add install for multer

--- server/app.js
+++ server/app.js
@@ -1,43 +1,44 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
-
+const cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json({message: 'DragMe Upload Image BE'});
+  res.status(200).json({ message: "DragMe Upload Image BE" });
 });
-
-const cloudinary = require('cloudinary');
-
 
 cloudinary.config({
-  cloud_name: 'dragme',
+  cloud_name: "dragme",
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
-const fileUpload = require('express-fileupload');
 
 app.use(fileUpload());
 
-app.post('/upload', (req, res) => {
-  console.log('heroku test api', process.env.CLOUDINARY_API_KEY)
-  const newPhoto = req.files['photo'].data.toString('base64');
-  const type = req.files['photo'].mimetype;
-  console.log('heroku check', req.files)
+app.post("/upload", (req, res) => {
+  console.log("heroku test api", process.env.CLOUDINARY_API_KEY);
+  const newPhoto = req.files["photo"].data.toString("base64");
+  const type = req.files["photo"].mimetype;
+  console.log("heroku check", req.files);
 
-  cloudinary.v2.uploader.upload(`data:${type};base64,${newPhoto}`, (err, photo) => {
-    if (err) {
-      console.error(err);
-      res.status(400).send(err);
-    } else {
-      const photoUrl = photo.url;
-      res.status(201).json({photoUrl});
+  cloudinary.v2.uploader.upload(
+    `data:${type};base64,${newPhoto}`,
+    (err, photo) => {
+      if (err) {
+        console.error(err);
+        res.status(400).send(err);
+      } else {
+        const photoUrl = photo.url;
+        res.status(201).json({ photoUrl });
+      }
     }
-  })
-})
+  );
+});
 
-module.exports = app
+module.exports = app;
